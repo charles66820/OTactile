586ea360e414a29e3dc40b61facf894d1ab14eeb
charles66820 <charles.goedefroit@gmail.com>
2019-04-15 03:53:10 +0200

    "login"

--- app/src/main/java/fr/magicorp/OTactile/ProfileFragment.java
+++ app/src/main/java/fr/magicorp/OTactile/ProfileFragment.java
@@ -9,10 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class ProfileFragment extends Fragment {
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
