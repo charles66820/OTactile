883191521f0c8cba24f66a5c152a06c1a41c98f3
charles66820 <charles.goedefroit@gmail.com>
2019-04-17 20:02:03 +0200

    profile IHM

--- app/src/main/java/fr/magicorp/OTactile/ProfileActivity.java
+++ app/src/main/java/fr/magicorp/OTactile/ProfileActivity.java
@@ -1,14 +1,42 @@
 package fr.magicorp.OTactile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // theme
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme);
+        } else {
+            setTheme(R.style.AppTheme);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        TableLayout tableLayout = (TableLayout)findViewById(R.id.addressesTable);
+
+        for (int i=0;i<6;i++){
+            View tableRow = LayoutInflater.from(this).inflate(R.layout.address_table_item,null,false);
+            TextView history_display_no  = (TextView) tableRow.findViewById(R.id.way);
+            TextView history_display_date  = (TextView) tableRow.findViewById(R.id.complement);
+            TextView history_display_orderid  = (TextView) tableRow.findViewById(R.id.postalCode);
+            TextView history_display_quantity  = (TextView) tableRow.findViewById(R.id.city);
+
+            history_display_no.setText(""+(i+1));
+            history_display_date.setText("2014-02-05");
+            history_display_orderid.setText("S0"+(i+1));
+            history_display_quantity.setText(""+(20+(i+1)));
+            tableLayout.addView(tableRow);
+        }
     }
 }
--- app/src/main/java/fr/magicorp/OTactile/ProfileFragment.java
+++ app/src/main/java/fr/magicorp/OTactile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package fr.magicorp.OTactile;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+         View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        final TextView login = (TextView) v.findViewById(R.id.login);
+        Button button = (Button) v.findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getContext(), ProfileActivity.class);
+                Bundle b = new Bundle();
+                b.putString("login", String.valueOf(login.getText()));
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+
+        return v;
     }
 }
