a56a22fe236034bd0c25cb4dc51fae6729ec51be
charles66820 <charles.goedefroit@gmail.com>
2019-04-18 01:20:10 +0200

    order IHM

---
+++ app/src/main/java/fr/magicorp/OTactile/ExpandableListAdapter.java
@@ -0,0 +1,107 @@
+package fr.magicorp.OTactile;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private final Context mContext;
+    private final List<Order> listDataOrder;
+    private HashMap<Order,List<OrderProduct>> listHashOrderProducts;
+
+    public ExpandableListAdapter(Context mContext, List<Order> listDataOrder, HashMap<Order, List<OrderProduct>> listHashOrderProducts) {
+        this.mContext = mContext;
+        this.listDataOrder = listDataOrder;
+        this.listHashOrderProducts = listHashOrderProducts;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listDataOrder.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return listHashOrderProducts.size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return listDataOrder.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return listHashOrderProducts.get(getGroup(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        Order order = (Order)getGroup(groupPosition);
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.order_list_item, null);
+        }
+
+        TextView no = (TextView) convertView.findViewById(R.id.noOrder);
+        TextView total = (TextView) convertView.findViewById(R.id.totalOrder);
+        TextView shipping = (TextView) convertView.findViewById(R.id.shippingOrder);
+        TextView deliveryAddress = (TextView) convertView.findViewById(R.id.deliveryAddressOrder);
+
+        no.setText(String.valueOf(order.getId()));
+        total.setText(String.valueOf(order.getTotal()));
+        shipping.setText(String.valueOf(order.getShipping()));
+        deliveryAddress.setText(String.valueOf(order.getAddress()));
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final OrderProduct orderProduct = (OrderProduct)getChild(groupPosition,childPosition);
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.order_list_content_item, null);
+        }
+
+        TextView title = (TextView) convertView.findViewById(R.id.titleOrderedProduct);
+        TextView reference = (TextView) convertView.findViewById(R.id.referenceOrderedProduct);
+        TextView price = (TextView) convertView.findViewById(R.id.priceOrderedProduct);
+        TextView quantity = (TextView) convertView.findViewById(R.id.quantityOrderedProduct);
+
+        title.setText(String.valueOf(orderProduct.getTitle()));
+        reference.setText(String.valueOf(orderProduct.getReference()));
+        price.setText(String.valueOf(orderProduct.getPriceTTC()));
+        quantity.setText(String.valueOf(orderProduct.getQuantity()));
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
--- app/src/main/java/fr/magicorp/OTactile/OrdersActivity.java
+++ app/src/main/java/fr/magicorp/OTactile/OrdersActivity.java
@@ -1,14 +1,59 @@
 package fr.magicorp.OTactile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class OrdersActivity extends AppCompatActivity {
+    private ExpandableListView listView;
+    private ExpandableListAdapter listAdapter;
+    private List<Order> listDataOrder;
+    private HashMap<Order,List<OrderProduct>> listHashOrderProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // theme
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme);
+        } else {
+            setTheme(R.style.AppTheme);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+
+        listView = (ExpandableListView) findViewById(R.id.orderExpList);
+        initData();
+        listAdapter = new ExpandableListAdapter(this,listDataOrder, listHashOrderProducts);
+        listView.setAdapter(listAdapter);
+    }
+
+    private void initData() {
+        listDataOrder = new ArrayList<>();
+        listHashOrderProducts = new HashMap<>();
+
+        listDataOrder.add(new Order(22314,40,20,"2, test truc et chose pour test, 66000 Perpignan",0));
+        listDataOrder.add(new Order(46545,10,10,"2, test truc et chose pour test1, 66000 Perpignan",0));
+        listDataOrder.add(new Order(4298,400,5,"2, test truc et chose pour test2, 66000 Perpignan",0));
+        listDataOrder.add(new Order(94956,50,42,"2, test truc et chose pour test3, 66000 Perpignan",0));
+
+        List<OrderProduct> ltest1 = new ArrayList<>();
+        ltest1.add(new OrderProduct(0, "produit 1", "dzef efzfzef e", 21.54, 62));
+        ltest1.add(new OrderProduct(564, "produit 2", "dzef fere e", 245, 4545));
+        ltest1.add(new OrderProduct(5465, "produit 3", "dzef ef e", 345, 4));
+        ltest1.add(new OrderProduct(24, "produit 4", "dzef fe e", 5, 54));
+        ltest1.add(new OrderProduct(984, "produit 5", "dzef zefze e", 53, 7));
+
+        listHashOrderProducts.put(listDataOrder.get(0), ltest1);
+        listHashOrderProducts.put(listDataOrder.get(1), ltest1);
+        listHashOrderProducts.put(listDataOrder.get(2), ltest1);
+        listHashOrderProducts.put(listDataOrder.get(3), ltest1);
     }
 }
