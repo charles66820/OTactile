f18e1e51f096e5b3ec18f2f65dc7d6a3d7d159c5
charles66820 <charles.goedefroit@gmail.com>
2019-04-21 01:07:00 +0200

    message product not found

--- app/src/main/java/fr/magicorp/OTactile/ProductsFragment.java
+++ app/src/main/java/fr/magicorp/OTactile/ProductsFragment.java
@@ -45,7 +45,7 @@ public class ProductsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View v = inflater.inflate(R.layout.fragment_products, container, false);
+        final View v = inflater.inflate(R.layout.fragment_products, container, false);
 
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(requireActivity());
 
@@ -98,6 +98,9 @@ public class ProductsFragment extends Fragment {
 
                                 products.add(product);
                             }
+
+                            (v.findViewById(R.id.emptyProduct)).setVisibility(View.INVISIBLE);
+
                             adapter.notifyDataSetChanged();
                         } catch (final JSONException e) {
                             Toast.makeText(getActivity(),
